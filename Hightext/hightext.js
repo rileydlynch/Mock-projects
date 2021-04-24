a77a3632e876a362c53e7e381e3354eed33dc3ba
Riley Lynch <rileydlynch@yahoo.com>
2021-04-24 22:51:02 +0800

    Update hightext.js
    Changed code so it uses `.getSelection()` when clicking on a button.
    
    コードはボタンをクリくする時に「getSelection()」は使います。

--- Hightext/hightext.js
+++ Hightext/hightext.js
@@ -1,17 +1,12 @@
-document.getElementById("demo").addEventListener("click", getSelectionText);
-
-window.getSelection().anchorNode.parentNode
+document.getElementById("button1").addEventListener("click", getSelectionText);
 
 function getSelectionText() {
     var text = "";
     if (window.getSelection) {
         text = window.getSelection().toString();
+		alert(text);
     } else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
+		alert(text);
     }
-    return text;
 };
-
-var hightext = getSelectionText();
-
-alert(hightext);
