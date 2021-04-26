7d8b527d5005b2f3d5ab03237872e4863e1bf64e
Riley Lynch <rileydlynch@yahoo.com>
2021-04-26 22:10:50 +0800

    Update hightext.js
    Added function to highlight text in test EJS page.
    
    テストのEJSのページで言葉をハイライトする関数を追加しました。

--- Hightext/hightext.js
+++ Hightext/hightext.js
@@ -3,10 +3,16 @@ document.getElementById("button1").addEventListener("click", getSelectionText);
 function getSelectionText() {
     var text = "";
     if (window.getSelection) {
-        text = window.getSelection().toString();
-		alert(text);
+        text = window.getSelection().toString(); //assigns selected text to variable
+		alert(text); //alerts selected text
+		document.getElementById("mytext").innerHTML = document.getElementById("mytext").innerHTML.replace(text,"<mark>" + text + "</mark>"); //puts <mark> around selected text, thus highlighting text. https://stackoverflow.com/questions/31872270/javascript-replace-text-in-an-element
     } else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
 		alert(text);
+		document.getElementById("mytext").innerHTML = document.getElementById("mytext").innerHTML.replace(text,"<mark>" + text + "</mark>");
     }
 };
+
+
+
+//document.getSelection()//.anchorNode.parentNode
