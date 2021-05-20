62d061455ea1709b3e7dd4c33edba055054e4df4
Riley Lynch <rileydlynch@yahoo.com>
2021-05-20 18:28:18 +0800

    Update state-map.js
    Added option to upload, but it's not properly connected to the graph yet.
    
    アップロードの関数を追加しましたけど、まだ正しくグラフと接続しません。

--- Flask-site/state-map.js
+++ Flask-site/state-map.js
@@ -1,27 +1,29 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 from urllib.request import urlopen
 import json
 with urlopen('https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_500k.json') as response:
-    states = json.load(response) //This imports GeoJSON data that is used to draw polygons on top of a map, in this case polygons of US states.
+    states = json.load(response) #//This imports GeoJSON data that is used to draw polygons on top of a map, in this case polygons of US states.
 	
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-import pandas as pd //This step imports a csv with the data you want to display on the map
-df = pd.read_csv("https://raw.githubusercontent.com/rileydlynch/Mock-projects/main/Flask-site/popdata.csv",
+import pandas as pd #//This step imports a csv with the data you want to display on the map
+df = pd.read_csv("https://raw.githubusercontent.com/rileydlynch/Mock-projects/main/Flask-site/nodata.csv",
                    dtype={"Geoid": str})
+locatvar = 'Geoid'
+colorvar = 'CENSUS2019POP'
 
 import plotly.express as px
 
-fig = px.choropleth_mapbox(df, geojson=states, locations='Geoid', featureidkey="properties.GEO_ID",
-                           color='CENSUS2019POP',
-                           color_continuous_scale="Viridis",
+fig = px.choropleth_mapbox(df, geojson=states, locations=locatvar, featureidkey="properties.GEO_ID",
+                           color=colorvar,
+                           color_continuous_scale="bluered",
 						   hover_name="NAME",
                            range_color=(0, 39512223),
                            mapbox_style="carto-positron",
@@ -30,10 +32,36 @@ fig = px.choropleth_mapbox(df, geojson=states, locations='Geoid', featureidkey="
                            labels={'unemp':'unemployment rate'}
                           )
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show() //Everything contained in the "fig" variable defines the map, including where it draws its data from.
+fig.show() #//Everything contained in the "fig" variable defines the map, including where it draws its data from.
 
-app.layout = html.Div([ //The layout defines and actually calls all of the below mentioned HTML objects.
-    dcc.Dropdown(#//This div is a dropdown containing choices of all 50 US states.
+app.layout = html.Div([ #//The layout defines and actually calls all of the below mentioned HTML objects.
+    html.H1("How To Use This Page"),
+	html.P("You have two options: Either upload your own CSV file containing the data you would like to use, or manually upload the data using the provided State dropdown and entering your data in the data input field."),
+	html.Br(),
+	html.H2("Option #1: Upload"),#//This begins the upload section
+	dcc.Upload(
+        id='csv-upload',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '50%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=False
+    ),#//End of upload section
+	html.Br(),
+	html.H2("Option #2: Manually input data:"),
+    html.Div(id='output-data-upload'),
+	dcc.Dropdown(#//This div is a dropdown containing choices of all 50 US states.
         id='state-choice',
         options=[
 {'label': 'Alabama', 'value': 'Alabama'},
@@ -90,20 +118,47 @@ app.layout = html.Div([ //The layout defines and actually calls all of the below
         placeholder='Click here for a dropdown or type to search for a state.'
     ),
     html.Div(id='state-choice-output'), #//This div's value is blank, but its value after being changed is defined below under "update_output"
-    html.Div(["",
-              dcc.Input(id='user-data-input', placeholder='Input your data here', type='text')]),
     html.Br(),
+	html.Div(["",
+              dcc.Input(id='user-data-input', placeholder='Input your data here', type='text')]),
+	html.Button('Submit', id='submit-val'),
     html.Div(id='user-data-output'),
-	dcc.Graph(figure=fig),
-])
+	html.Br(),
+	html.Div(id='graph-output'),
+])#//End of layout
 
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+    return html.Div([dcc.Graph(figure = fig)])
 
 @app.callback(
     dash.dependencies.Output('state-choice-output', 'children'),
     [dash.dependencies.Input('state-choice', 'value')])
 def update_output(value):
     return 'You have selected "{}". Please enter your data below:'.format(value) #//Here is where the value of the 'state-choice-output' Div is updated.
-
+		
+@app.callback(Output('graph-output', 'children'),
+              Input('csv-upload', 'contents'),
+              State('csv-upload', 'filename'),
+              State('csv-upload', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 if __name__ == '__main__':
     app.run_server(debug=True)
